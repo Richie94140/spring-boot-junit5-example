ce529bd1db890a73bbd006ba2c5f44138f187c2f
unknown <eric.siry@gmail.com>
2022-09-21 11:10:55 +0200

    upload

---
+++ src/main/java/example/service/Calculatriceservice.java
@@ -0,0 +1,19 @@
+package example.service;
+
+public class Calculatriceservice {
+	
+public int somme(int x,int y)	{
+return x+y; 
+	
+	
+	
+	
+}
+
+public int soustraction(int x,int y)	{	
+return x-y; 
+	
+	
+}
+
+}
---
+++ src/test/java/example/service/test.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package example.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author orsys
+ *
+ */
+class test {
+	
+public Calculatriceservice calculatriceservice=new Calculatriceservice();
+	
+	
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	//@BeforeAll
+	void setUpBeforeClass()  {
+	
+	
+	
+	
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	//@AfterAll
+	void tearDownAfterClass()  {
+	}
+
+	@Test
+	void testSoustraction() {
+			
+		//etape1 initialisation des paramètres d'entrée
+				int a=27;
+				int b=8;
+			//étape2 appel du service à tester et stockage du résultat
+				int resultat=calculatriceservice.soustraction(a, b);
+				
+			//étape3 vérification assertion
+			//assertEquals(expected,actual);
+				assertEquals(19,resultat);
+				assertTrue(resultat>0);
+	}
+	
+	@Test
+	void testSomme() {
+		
+	//etape1 initialisation des paramètres d'entrée
+		int a=27;
+		int b=8;
+	//étape2 appel du service à tester et stockage du résultat
+		int resultat=calculatriceservice.somme(a, b);
+		
+	//étape3 vérification assertion
+	//assertEquals(expected,actual);
+		assertEquals(35,resultat); 
+		
+	}
+		
+	@Test
+		void testTemplate() {
+			
+		//etape1 initialisation des paramètres d'entrée
+			int a=27;
+			int b=8;
+		//étape2 appel du service à tester et stockage du résultat
+			int resultat=calculatriceservice.somme(a, b);
+			
+		//étape3 vérification assertion
+		//assertEquals(expected,actual);
+			assertEquals(35,resultat);
+				
+	
+	
+}}
